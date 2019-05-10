e19ac4d192a53e93429353738e5696adcf0ef148
Abelsjcheng <429433693@qq.com>
2019-05-10 14:46:01 +0800

    getter中添加图表数据配置信息

--- src/store/getters.js
+++ src/store/getters.js
@@ -6,5 +6,6 @@ const getters = {
     zdinfo: state => state.common.zdinfo,//终端故障标注
     zdinfocontent: state => state.common.zdinfocontent,//终端故障信息
     selectplace: state => state.common.selectplace,//地址选择器选择的地点
+    chartSet: state => state.common.chartSet,//图表数据配置
     }
 export default getters

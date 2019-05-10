0275b795e73d996bb418b5ac609ca0bf3180137a
Abelsjcheng <429433693@qq.com>
2019-05-10 14:47:00 +0800

    添加图表控制器

---
+++ src/components/Iot/ChartControl/rainchartcol.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div class="chartblock" v-if="rainvisible">
+                <div class="chart_title">雨量折线图 </div>
+                <div>
+                <i class="el-icon-setting" v-popover:popover4 style="color: #05d2e2;"></i>
+                <i class="el-icon-circle-close-outline" @click="rainvisible = false" style="color: #05d2e2;"></i>
+                </div>
+        </div>
+       <el-popover ref="popover4"  placement="bottom-start" width="420" v-model="popvisible">
+                <el-date-picker v-model="chartSet.timeframe" type="datetimerange" value-format="yyyy-MM-dd HH:mm:ss" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期"  align="right"  >
+                </el-date-picker>
+            <div class="block">
+                <span class="demonstration">查询数量</span>
+                <el-slider v-model="chartSet.limit"></el-slider>
+            </div>
+            <div style="text-align: right; margin: 0">
+                <el-button size="mini" type="text" @click="popvisible = false">取消</el-button>
+                <el-button type="primary" size="mini" @click="submit">确定</el-button>
+            </div>
+        </el-popover>
+    </div>  
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    name:"rainchartcol",
+    data(){
+        return {
+            popvisible: false,
+            rainvisible:this.rainchartshow,
+            chartSet:{name:"",limit:20,timeframe:["2019-02-01 10:10:00", "2019-04-18 10:10:00"]}
+        }
+    },
+    props:["rainchartshow"],
+    methods:{
+        formatTooltip(val) {
+            return val / 100;
+        },
+        submit(){
+            console.log(this.rainchartshow)
+            this.popvisible=false;
+            this.$store.commit("SET_CHARTSET",this.chartSet);
+        }
+    }
+}
+</script>
+

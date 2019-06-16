bd794cbd4a12e0367f347b0cce1de19c081af7f9
Abelsjcheng <429433693@qq.com>
2019-06-16 17:12:26 +0800

    iot图表设计

--- src/components/Iot/charts/piechart.vue
+++ src/components/Iot/charts/piechart.vue
@@ -1,62 +1,93 @@
 <template>
-      <v-chart  :options="polar" style="width:23vw;height:29vh"/>    
+    <div class="iot_char_right" v-show="chartvisible">
+      <v-chart  :options="polar" style="width:20vw;height:29vh"/>    
+    </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
 name: "piechart",
   data () {
     return {
-      polar: {
-        title : {
-            text: '终端:862105024041885最新数据',
-            x:'center',
-            textStyle:{
-            color: '#00ffb2'
-            }
-        },
-        tooltip : {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} (v)"
-        },
-        series : [
-            {
-                name: '终端采集数据',
-                type: 'pie',
-                radius : '70%',
-                center: ['50%', '55%'],
-                
-                data:[
-                    {value:33, name:'电池1电压'},
-                    {value:31, name:'电池组电压'},
-                    {value:23, name:'24v输出电压'},
-                    {value:23, name:'28v输出电压'},
-                    {value:13, name:'18v1输出电压'},
-                    {value:15, name:'18v2输出电压'},
-                    {value:15, name:'外部电压'},
-                    {value:15, name:'太阳能电压'}
+    polar: {
+            title: {
+                left: 'center',
+                text: '河水酸碱度',
+                textStyle:{
+                        color: '#ffffff'
+                        }
+            },
+            tooltip : {
+                        trigger: 'axis',
+                        axisPointer: {
+                        type: 'cross',
+                        animation: false,
+                        label: {
+                            backgroundColor: '#505765'
+                    }
+                }
+            },
+                dataZoom: [{
+                type: 'inside',
+                start: 0,
+                end: 100
+            },
                 ],
-                label: {
-                normal: {
-                    textStyle: {
-                        color: '#05d2e2'
-                    }
+            xAxis: {
+                type: 'category',
+                axisLine:{
+                                lineStyle:{
+                                color:'#ffffff',
+                                width:1,//这里是为了突出显示加上的
+                                }
+                            },
+                data: ['2018-04-21 12:37:14', '2018-04-22 12:37:14', '2018-04-23 12:37:14', '2018-04-24 12:37:14', '2018-04-21 12:37:14', '2018-04-21 12:37:14', '2018-04-21 12:37:14']
+            },
+            yAxis: {
+                type: 'value',
+                max:15,
+                axisLine:{
+                                lineStyle:{
+                                color:'#ffffff',
+                                width:1,//这里是为了突出显示加上的
+                                }
+                            },
+                data:[6,8,4,7,2,5,9,3]
+
+            },
+            series: [
+                {
+                    name:'酸碱度',
+                    type:'line',
+                    symbol: 'none',
+                    sampling: 'average',
+                    data:[6,8,4,7,2,5,9,3],
+                    itemStyle: {
+                        normal: {
+                            color: '#f2c31a'
+                        }
+                    },
+                    // areaStyle: {
+                    //     normal: {
+                    //         color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                    //             offset: 0,
+                    //             color: '#8ec6ad'
+                    //         }, {
+                    //             offset: 1,
+                    //             color: '#ffe'
+                    //         }])
+                    //     }
+                    // },
                 }
-                },
-                itemStyle: {
-                    emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }
-         ]
-       }
+            ]
+        },
+       chartvisible:false
+
     }
   },
    methods:{
-      getpowdata:function(){ //
+      getcoldata:function(){ //
               //发送get请求
                   
                   this.$http.get('http://localhost:8081/RiverVis/api/newpow?id=862105024024824').then(function (res) {
@@ -74,6 +105,25 @@ name: "piechart",
                       console.log(error);
                     });
              
+      }
+  },
+  computed: { //计算属性 取存在状态库中的值
+     ...mapGetters(["chartSet"]),
+     listenchartSet(){  //监听 chartSet值的变化
+       return this.chartSet;
+     }
+  },
+  watch:{
+      listenchartSet:{
+          handler(vag){  //handler执行具体方法
+            if(vag.name=='ph') //河水酸碱度
+            {
+                this.getcoldata(vag.timeframe[0],vag.timeframe[1],vag.limit);
+                this.chartvisible=vag.chartvisible;
+            }
+        },
+        deep: true//是否深度监听设置deep: true  则可以监听到vag.timeframe的变化，此时会给vag的所有属性都加上这个监听器，
+        //immediate: true 代表如果在 wacth 里声明了 变量 之后，就会立即先去执行里面的handler方法
       }
   },
   mounted:function(){//页面初始化函数

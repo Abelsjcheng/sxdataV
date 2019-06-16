b2f398a5db9224ac17bc213c36f2d4dcdb728d3c
Abelsjcheng <429433693@qq.com>
2019-06-16 17:11:40 +0800

    新建预警模块

---
+++ src/components/Iot/item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div style="height:20px">
+        1
+    </div>
+</template>
+<script>
+    
+    export default {
+        data () {
+            return {
+                
+            }
+        }
+    }
+</script>

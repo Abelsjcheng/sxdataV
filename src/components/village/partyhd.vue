7166de5fa98fa44ac4a6025b18778763fec0739a
dyq <1069016546@qq.com>
2019-06-23 17:40:06 +0800

    改回原先api

--- src/components/village/partyhd.vue
+++ src/components/village/partyhd.vue
@@ -38,7 +38,7 @@
         
         getPhddata:function(){ 
 
-                this.$http.get('http://www.teavamc.com/api/policy/ten').then(function(res){  
+                this.$http.get('http://110.53.162.165:5050/api/party/all').then(function(res){  
                                 for (let i = 0; i < res.data.data.length; i++) {
                                     const info={date:res.data.data[i].hdadddate,title:res.data.data[i].hdtitle,hdtype:res.data.data[i].hdtype,hdloc:res.data.data[i].hdloc,hdnum:res.data.data[i].hdnum,hdbtime:res.data.data[i].hdbtime,hdetime:res.data.data[i].hdetime,hdsum:res.data.data[i].hdsum,hdpic:res.data.data[i].hdpic}
                                     this.tableData.push(info) //存入json数组

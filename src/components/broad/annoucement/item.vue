1c7cc6fe2fd6204a256e87aba9b8098336f1c3ba
Abelsjcheng <429433693@qq.com>
2019-07-17 14:42:07 +0800

    修改终端故障公告

---
+++ src/components/broad/annoucement/item.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="dataitem" @click="showcontent(item)">
+        <div class="top"> 
+            <span class="data_title">{{item.tid}} </span>
+            <el-badge :type="item.badge" class="data_level"  ></el-badge>
+            <span class="data_time">{{item.ctime}} </span>
+        </div>
+        <div class="data_content">
+            {{item.content}}
+        </div>
+    </div>
+</template>
+<script>
+    import { mapGetters } from 'vuex';
+    export default {
+        data () {
+            return {
+                
+            }
+        },
+        props:['item'],
+        methods:{
+            showcontent (message) {
+            //赋值
+            this.infocontent={lo:message.lo,la:message.la,type:message.type,tid:message.tid,content:message.content,ctime:message.ctime}
+            this.$store.commit('SET_ZDINFOCONTENT',this.infocontent); //提交至状态库存储用于标注弹窗
+            //this.$refs.mycontent.show()//显示弹窗
+            },
+        }
+    }
+</script>
+<style lang="scss">
+.dataitem{
+    border-bottom: 1px solid #eee;;
+    background-color: rgba(20,66,125,0.12);
+    height: 80px;
+    margin-bottom: 6px;
+    color: white;
+}
+.top{
+    height: 25px;
+}
+
+
+.data_level {
+    float: left;
+    color: white;
+    font-size: 12px;
+    text-align: center;
+}
+.el-badge__content{
+    height: 22px;   
+    padding: 0 10px;
+    border:none;
+    margin-left: 8px;
+    margin-top:3px;
+}
+.data_title{
+    float: left;
+    background-image: url(../../../static/img/pushmessage_class.png);
+    background-repeat: no-repeat;
+    width: 150px;
+    line-height: 25px;
+    color: white;
+    font-size: 14px;
+    text-align: center;
+}
+.data_content {
+    padding-left: 10px;
+    padding-right: 6px;
+    overflow: hidden;
+    margin-top: 8px;
+}
+
+.localize {
+    display: block;
+    line-height: 25px;
+    margin-left: 8px;
+    background-image: url(../../../static/img/loca.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position-y: 3px;
+    width: 20px;
+    height: 28px;
+    float: left;
+}
+.data_time{
+    color: #4a97da;
+    font-size: 12px;
+    line-height: 25px;
+    text-align: right;
+    display: block;
+    float: right;
+    margin-right: 5px;
+}
+</style>
--- src/components/broad/annoucement/zdinfo.vue
+++ src/components/broad/annoucement/zdinfo.vue
@@ -3,54 +3,31 @@
       <h2 style="color:#ffffff;text-align:center;">公告</h2>
       <!--公告列表框 -->
       <virtual-list :size="60" :remain="8">
-           <b-list-group >
-            <b-list-group-item v-for="(item, index) in annoucements" :key="index" @click="showcontent(item)" style="background-color: rgba(255,255,255,0);border-bottom: 1px solid #eee;">
-              <b-badge :variant="item.variant" style="float: left;" pill class="px-3 py-2">{{item.badge}}</b-badge> 
-              <span style="color:#ffffff;" class="pl-4">{{item.content}}</span> 
-            </b-list-group-item>
-          </b-list-group>
+        <item v-for="item of annoucements" :key="item.id" v-bind:item="item"  />
       </virtual-list>
-       <!--信息弹窗 -->
-      <b-modal ref="mycontent"  hide-footer :title="'终端号:'+infocontent.tid" >
-        <h2 style="text-align:center;">终端故障 </h2>
-        <div class="d-block ">
-          <h4 v-html="infocontent.content"></h4> <!--v-html 输出文本对带html标签进行解析 -->
-        </div>
-        <h6 style="text-align:right;">{{infocontent.ctime}} </h6>
-        <b-btn class="mt-3" variant="outline-danger" block @click="hidecontent">关闭</b-btn>
-      </b-modal>
     </div>  
 </template>
 
 <script>
-
+import item from './item.vue'
 import virtualList from 'vue-virtual-scroll-list'//列表滚动组件
 export default {
-components: {'virtual-list': virtualList },
+components: {'virtual-list': virtualList,item },
 name: "zdinfo",
   data () {
     return {
      annoucements:[],      //badge：置顶/紧急/普通;variant:warning/danger/info;title json数组
-     info:{badge:'',variant:'',tid:'',content:'',ctime:'',lo:'',la:'',type:''}, //进行双向绑定 tid:终端id content：终端故障原因
-     infocontent:{tid:'',content:'',ctime:'',lo:'',la:''} //信息弹窗内容
+     info:{badge:'danger',tid:'',content:'',ctime:'',lo:'',la:'',type:''}, //进行双向绑定 tid:终端id content：终端故障原因
     }
   },
   methods: {
-    showcontent (message) {
-      //赋值
-      this.infocontent={lo:message.lo,la:message.la,type:message.type,tid:message.tid,content:message.content,ctime:message.ctime}
-      this.$store.commit('SET_ZDINFOCONTENT',this.infocontent); //提交至状态库存储用于标注弹窗
-      //this.$refs.mycontent.show()//显示弹窗
-    },
-    hidecontent () {
-      this.$refs.mycontent.hide()//隐藏弹窗
-    },
+    
     get:function(){ 
             //发送get请求
             this.$http.get('http://110.53.162.165:5050/api/device/mlal').then(function(res){
                         
                           for (let i = 0; i < res.data.data.length; i++) {
-                            this.info={badge:'普通',variant:'info',tid:res.data.data[i].tid,content:res.data.data[i].content,ctime:res.data.data[i].ctime,lo:res.data.data[i].lo,la:res.data.data[i].la,type:'停止'}
+                            this.info={id:i,badge:'danger',tid:res.data.data[i].tid,content:res.data.data[i].content,ctime:res.data.data[i].ctime,lo:res.data.data[i].lo,la:res.data.data[i].la,type:'停止'}
                             this.annoucements.push(this.info) //存入json数组
                           }
                           this.$store.commit('SET_ZDINFO',this.annoucements);
